e60082db1dadefd0db10e2be53f02fc6d1f80e01
Niko Pietikäinen <niko.pietikainen@gmail.com>
2020-08-04 22:12:22 +0200

    Add unary operators

--- _parser.py
+++ _parser.py
@@ -18,6 +18,12 @@ class Num(AST):
         self.value = token.value
 
 
+class UnaryOp(AST):
+    def __init__(self, op, expr):
+        self.token = self.op = op
+        self.expr = expr
+
+
 class Parser:
     def __init__(self, lexer):
         self.lexer = lexer
@@ -34,7 +40,15 @@ class Parser:
 
     def factor(self):
         token = self.current_token
-        if token.type == INTEGER:
+        if token.type == PLUS:
+            self.eat(PLUS)
+            node = UnaryOp(token, self.factor())
+            return node
+        elif token.type == MINUS:
+            self.eat(MINUS)
+            node = UnaryOp(token, self.factor())
+            return node
+        elif token.type == INTEGER:
             self.eat(INTEGER)
             return Num(token)
         elif token.type == LPAREN:
@@ -60,7 +74,7 @@ class Parser:
 
         expr    : term ((PLUS | MINUS) term)*
         term    : factor ((MUL | DIV) factor)*
-        factor  : INTEGER | LPAREN expr RPAREN
+        factor  : (PLUS|MINUS) factor | INTEGER | LPAREN expr RPAREN
         """
         node = self.term()
         while self.current_token.type in (PLUS, MINUS):
