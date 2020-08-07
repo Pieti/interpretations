40465e7b1014624c55f34936b76b5c07a851a00f
Niko Pietikäinen <niko.pietikainen@gmail.com>
2020-08-07 10:14:38 +0200

    Add sample pascal program

---
+++ testprogram.pas
@@ -0,0 +1,16 @@
+BEGIN {Part10}
+   BEGIN
+      number := 2;
+      a := number;
+      b := 10 * a + 10 * number DIV 4;
+      c := a - - b
+   END;
+   x := 11;
+   y := 20 / 7 + 3.14;
+   { writeln('a = ', a); }
+   { writeln('b = ', b); }
+   { writeln('c = ', c); }
+   { writeln('number = ', number); }
+   { writeln('x = ', x); }
+   { writeln('y = ', y); }
+END.  {Part10}
